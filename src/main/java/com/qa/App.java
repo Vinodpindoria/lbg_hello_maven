7c658aff55295150e5c496923489c00ff44cc696
vinodpindoria <vinodpindoria85@gmail.com>
2026-02-10 15:07:09 +0000

    EDIT: Changed snapshot version and added a new line of code

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,8 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Vinod"));
+        System.out.println(sayHelloToSomeone("Mark"));
         System.out.println(sayGoodbye());
     }
 
